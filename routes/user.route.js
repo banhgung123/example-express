6b3b141c98d35f8df2758055edda45cbb2dad694
tri <banhgung@gmail.com>
2021-02-01 06:20:34 +0700

    first commit

---
+++ routes/user.route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const controller = require('./../controllers/user.controller');
+const validate = require('./../validate/user.validate');
+
+const router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/cookie', (req, res, next) => {
+    res.send('Hello');
+});
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', validate.postCreate, controller.postCreate);
+
+module.exports = router;
